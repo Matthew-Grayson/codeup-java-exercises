a2748d731cd4320d2c98e9895604c7c83687e644
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-01 16:57:47 -0500

    completed syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,26 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        int myFavoriteNumber = 6;
+        String myString = "As God as my witness, he is broken in half.";
+        float myNumber;
+        myNumber = (float) 3.14;
+        System.out.print(myNumber);
+        System.out.print("\nHello, World!\n");
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
     }
 }
