494cd5274bdf54098dad501991c658aff6b94169
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-02 14:45:19 -0500

    do-while pow 2-65536

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 100;
+        int i = 2;
         do {
             System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
+            i = (int) Math.pow(i,2);
+        } while (i < 100000);
     }
 }
