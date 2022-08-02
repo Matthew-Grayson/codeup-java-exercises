580d8bc125cf387daa5ecfe7e331c78ad988bf6a
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-02 14:57:08 -0500

    for loop pow 2-65536

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,7 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 100; i >= -10; i -= 5) {
-            System.out.print(i + " ");
+        for(int i = 2; i <= 100000; i = (int) Math.pow(i,2)) {
+            System.out.print(i + " ");d
         }
     }
 }
