92d9555310eb4587c2f66141d30984ff893e19e0
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-02 15:08:42 -0500

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,13 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 2; i <= 100000; i = (int) Math.pow(i,2)) {
-            System.out.print(i + " ");d
+        for(int i = 1; i <= 100; i++) {
+            if(i % 15 == 0) {
+                System.out.println(i + " fizzbuzz");
+            } else if (i % 3  == 0) {
+                System.out.println(i + " fizz");
+            } else if(i % 5 == 0) {
+                System.out.println(i + " buzz");
+            } else System.out.println(i);
         }
     }
 }
