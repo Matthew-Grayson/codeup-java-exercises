b6352a34398f948a1a05c2ab236d24040c717784
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-02 15:36:24 -0500

    display table of powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,20 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 1; i <= 100; i++) {
-            if(i % 15 == 0) {
-                System.out.println(i + " fizzbuzz");
-            } else if (i % 3  == 0) {
-                System.out.println(i + " fizz");
-            } else if(i % 5 == 0) {
-                System.out.println(i + " buzz");
-            } else System.out.println(i);
+        Scanner powers = new Scanner(System.in);
+        System.out.print("Continue? [y/n] ");
+        String userChoice = powers.next();
+        if(Objects.equals(userChoice, "y")) {
+            System.out.print("What number would you like to go up to? ");
+            int userInput = powers.nextInt();
+            for(int i = 1; i <= userInput; i++) {
+                System.out.print(i + "^2 = ");
+                System.out.format("%.0f; ", Math.pow(i,2));
+                System.out.print(i + "^3 = ");
+                System.out.format("%.0f%n", Math.pow(i,3));
+            }
         }
     }
 }
